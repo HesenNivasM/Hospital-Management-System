71bd3d1cd19a5c4ff70d9c55a9df1d4da0fe77d5
HesenNivasM <hesennivas@gmail.com>
2018-10-13 00:54:03 +0530

    first commit

---
+++ dk.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 12, 2018 at 09:23 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blockchain`
+--
+
+CREATE TABLE `blockchain` (
+  `id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `dates` date NOT NULL,
+  `timing` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `blockchain`
+--
+
+INSERT INTO `blockchain` (`id`, `doctor_id`, `patient_id`, `dates`, `timing`) VALUES
+(3, 3, 5, '2018-04-25', '2.00PM'),
+(4, 2, 5, '2018-04-07', '4.00PM'),
+(6, 77, 7, '2018-04-28', '2.00PM'),
+(7, 3, 8, '2018-04-13', '12.00PM'),
+(8, 3, 9, '2018-04-28', '1.00PM'),
+(9, 1833556, 10, '2018-05-17', '9.00AM'),
+(10, 16161616, 11, '2018-05-30', '2.00PM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_details`
+--
+
+CREATE TABLE `doctor_details` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `qual` varchar(255) NOT NULL,
+  `special` varchar(255) NOT NULL,
+  `phn` bigint(20) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctor_details`
+--
+
+INSERT INTO `doctor_details` (`id`, `name`, `qual`, `special`, `phn`, `username`, `password`) VALUES
+('1', 'Dinesh', 'MBBS,MS', 'Cardiology', 8190038383, 'dinesh', 'dinesh'),
+('16161616', 'Selva Rani', 'MBBS MD MD', 'Neurosurgery', 982501921, 'selvarani', 'selvarani'),
+('1833556', 'Monisha', 'MBBS MD', 'General Surgery', 8190038383, 'monisha', 'monisha'),
+('2', 'Gowtham', 'MBBS', 'Anaesthesiology', 8190038383, 'gowtham', 'gowtham'),
+('3', 'Hesen', 'MBBS,MS', 'Cardiology', 8190038383, 'hesen', 'hesen'),
+('77', 'Aakash', 'MBBS MS', 'General Medicine', 82191282721, 'aakash', 'aakash');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_details`
+--
+
+CREATE TABLE `patient_details` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `sex` varchar(20) NOT NULL,
+  `phn` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient_details`
+--
+
+INSERT INTO `patient_details` (`id`, `name`, `age`, `sex`, `phn`) VALUES
+(1, 'Hesen', 19, 'Male', 8190038383),
+(5, 'Dinesh', 19, 'Male', 9443854160),
+(7, 'bharath', 19, 'Male', 7777777777),
+(8, 'aravind', 19, 'Female', 8881118889),
+(9, 'eli', 19, 'Female', 19181918191),
+(10, 'Hesen', 19, 'Male', 8124432151),
+(11, 'Rakesh', 17, 'Male', 9090999090);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blockchain`
+--
+ALTER TABLE `blockchain`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctor_details`
+--
+ALTER TABLE `doctor_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patient_details`
+--
+ALTER TABLE `patient_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blockchain`
+--
+ALTER TABLE `blockchain`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `patient_details`
+--
+ALTER TABLE `patient_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
